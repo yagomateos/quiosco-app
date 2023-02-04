45579307cf2f64cdde9e881e96f9159633d296b8
Yago <yagomateos@hotmail.com>
2023-02-04 21:01:29 +0100

    not working yet

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,19 @@
-import { categorias } from "./data/categorias";
-import { productos } from "./data/productos";
-import { PrismaClient } from "@prisma/client";
+import { categorias } from './data/categorias'
+import { productos} from './data/productos'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const main = async() : Promise<void> => {
-  try {
-    await prisma.categoria.createMany({
-      data: categorias
-    })
-    await prisma.producto.createMany({
-      data: productos
-    })
-  } catch(error) {
-    console.log(error)
-  }
+const main = async () : Promise<void> => {
+    try {
+        await prisma.categoria.createMany({
+            data: categorias
+        })
+        await prisma.producto.createMany({
+            data: productos
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
 main()
